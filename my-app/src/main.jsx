b99cae9f1daadd0f7b3722175ee6ccf37a732b14
lazymaster101 <seshadithya01@gmail.com>
2025-03-28 11:30:23 -0500

    cashier v1

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Cashier from './Cashier.jsx'
+import 'flowbite';
+import 'flowbite-react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Cashier>
+      
+    </Cashier>
   </StrictMode>,
 )
