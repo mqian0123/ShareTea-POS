35be4bfaafa7170752223c72a6f8f8496b8a79c6
lazymaster101 <seshadithya01@gmail.com>
2025-03-28 12:32:23 -0500

    cashier v1.1

--- my-app/src/Cashier.jsx
+++ my-app/src/Cashier.jsx
@@ -51,10 +51,18 @@ function Cashier () {
             </div>
             <div class = "flex m-5">
                 <div class = "flex flex-col w-3/5 mr-5">
-                    <div>
-                        <input type = 'search' id = "searchBar" class = "w-full border-2 border-emerald-800 rounded-full mb-5 font-sans text-black h-10 bg-white" placeholder='Search'/>
-                    </div>
-
+                    <form class="max-w-full">   
+                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                        <div class="relative">
+                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                </svg>
+                            </div>
+                            <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                            <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-emerald-700 hover:bg-emerald-900 focus:ring-4 focus:outline-none focus:ring-emerald-50 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                        </div>
+                    </form>
                     <div class = "flex overflow-auto">
                         <Categories status="Available" categoryName = "Fruit Tea">
                         </Categories>
@@ -65,7 +73,10 @@ function Cashier () {
 
                         </Categories>
                         <Categories status = "Available" categoryName = "Ice Blended">
-
+                        </Categories>
+                        <Categories status = "Available" categoryName = "Tea Mojito">
+                        </Categories>
+                        <Categories status = "Available" categoryName = "Creama">
                         </Categories>
                     </div>
 
