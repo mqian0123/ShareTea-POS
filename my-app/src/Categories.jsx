5c85d36f341c6c72f4066c123dda95fa2700adda
lazymaster101 <seshadithya01@gmail.com>
2025-04-09 14:43:39 -0500

    changed styling for cateogries buttons in cashier side

--- my-app/src/Categories.jsx
+++ my-app/src/Categories.jsx
@@ -11,18 +11,18 @@ function Categories (props) {
         
         <button 
         onClick = {props.onClick} 
-        className = "hover:cursor-pointer w-auto m-5 inline-block bg-emerald-900 border rounded-4xl py-4 pr-20 pl-5">
+        className = {props.className}>
 
             {/* //TODO: Change the status dynamically */ }
-            <div className = "border whitespace-nowrap border-white rounded-full text-white text-xs py-1 px-3">
+            <div className = "whitespace-nowrap rounded-full text-xs py-1 px-3">
                 {props.status}
             </div>
-            <p className = "font-bold text-white mt-5 whitespace-nowrap">
+            <p className = "font-bold mt-5 whitespace-nowrap">
                 {props.categoryName}
             </p>
 
             {/* //TODO: Change the number of items dynamically */ }
-            <p className = "text-xs text-white whitespace-nowrap">
+            <p className = "text-xs whitespace-nowrap">
                 50 items
             </p>
         </button>
