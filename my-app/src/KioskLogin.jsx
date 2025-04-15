b8f2d16f44c13ad6240038e09414bbcb101602d1
lazymaster101 <seshadithya01@gmail.com>
2025-04-14 23:32:39 -0500

    kiosk login screen prototype v2

--- my-app/src/KioskLogin.jsx
+++ my-app/src/KioskLogin.jsx
@@ -34,40 +34,13 @@ function KioskLogin () {
                             By using your rewards account
                         </p>
                 </button>
-                <button className='hover:cursor-pointer hover:-translate-y-0.5 border bg-white rounded shadow-md border-gray-100 font-bold flex items-center justify-center'>
-                    <img src={languageIcon} className='w-27'>
-                    </img>
-                    <p>
-                        Languages
-                    </p>
-                </button>
                 <button className = "hover:cursor-pointer hover:-translate-y-0.5 border bg-white rounded shadow-md border-gray-100 font-bold flex items-center justify-center">
                     <img src={accessiblityIcon} className='w-20'>
                     </img>
                         Accessibility
                 </button>
+                <div id="google_translate_element" className='bg-white'></div>
 
-                    {/* <div className='flex flex-col w-3/5  gap-5'>
-                        <div className='flex gap-5'>
-                            <button className='border w-1/2 bg-white rounded shadow-md border-gray-100 font-bold flex items-center justify-center'>
-                                <img src={languageIcon} className='w-25'>
-                                </img>
-                                <p>
-                                    Languages
-                                </p>
-                            </button>
-                            <button className = "border w-1/2 bg-white rounded shadow-md border-gray-100 font-bold flex items-center justify-center">
-                            <img src={accessiblityIcon} className='w-25'>
-                            </img>
-                                Accessibility
-                            </button>
-                        </div>
-                    </div> */}
-                {/* <div className=''>
-                    <button className='border h-full bg-white rounded shadow-md border-gray-100 font-bold inline-block p-5'>
-                        Earn Points
-                    </button>
-                </div> */}
                 </div>
             </div>
         </>
